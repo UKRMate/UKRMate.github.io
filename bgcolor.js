835f97484daa319b11ea5f958de65ba3cf6a4b37
UKRMate <zarazazarazovich7@gmail.com>
2023-08-16 01:11:58 +0300

    бордер

--- bgcolor.js
+++ bgcolor.js
@@ -1,27 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const darkColors = ["#001f3f", "#003366", "#004080", "#006400", "#8B0000", "#800080"];
-  const randomDarkColor = darkColors[Math.floor(Math.random() * darkColors.length)];
+  const backgroundColor = "#00008B"; // Колір фону сайту
+  const borderColor = "#0000CD"; // Колір обводки
 
-  document.body.style.backgroundColor = randomDarkColor;
-
-  const brighterColor = getBrighterColor(randomDarkColor, 30); // Отримати яскравіший колір
-  document.body.style.borderColor = brighterColor;
+  document.body.style.backgroundColor = backgroundColor;
+  document.body.style.borderColor = borderColor;
 
   const header = document.querySelector(".header");
-  header.style.backgroundColor = randomDarkColor;
-  header.style.borderColor = brighterColor;
+  header.style.backgroundColor = backgroundColor;
+  header.style.borderColor = borderColor;
 });
-
-function getBrighterColor(color, percent) {
-  const hex = parseInt(color.slice(1), 16);
-  const r = (hex >> 16) & 255;
-  const g = (hex >> 8) & 255;
-  const b = hex & 255;
-  const amt = percent / 100;
-
-  const newR = Math.min(255, Math.round(r + 255 * amt));
-  const newG = Math.min(255, Math.round(g + 255 * amt));
-  const newB = Math.min(255, Math.round(b + 255 * amt));
-
-  return `rgb(${newR}, ${newG}, ${newB})`;
-}
