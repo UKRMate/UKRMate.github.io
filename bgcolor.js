d9dda74a40f008745b18e5e84836bb8e4ed9a676
UKRMate <zarazazarazovich7@gmail.com>
2023-08-16 00:35:35 +0300

    кольори

--- bgcolor.js
+++ bgcolor.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const darkColors = ["#001f3f", "#003366", "#004080", "#006400", "#8B0000", "#800080"];
   const randomDarkColor = darkColors[Math.floor(Math.random() * darkColors.length)];
-  
+
   document.body.style.backgroundColor = randomDarkColor;
-  document.body.style.borderColor = lightenColor(randomDarkColor, 30); // Застосування яскравішого кольору
-  
+
+  const brighterColor = getBrighterColor(randomDarkColor, 30); // Отримати яскравіший колір
+  document.body.style.borderColor = brighterColor;
+
   const header = document.querySelector(".header");
   header.style.backgroundColor = randomDarkColor;
-  header.style.borderColor = lightenColor(randomDarkColor, 30); // Застосування яскравішого кольору
+  header.style.borderColor = brighterColor;
 });
 
-function lightenColor(color, percent) {
-  const num = parseInt(color.replace("#", ""), 16);
-  const amt = Math.round(2.55 * percent);
-  const R = (num >> 16) + amt;
-  const G = (num >> 8 & 0x00FF) + amt;
-  const B = (num & 0x0000FF) + amt;
-  return `#${(0x1000000 + (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 + (B < 255 ? (B < 1 ? 0 : B) : 255)).toString(16).slice(1)}`;
+function getBrighterColor(color, percent) {
+  const hex = parseInt(color.slice(1), 16);
+  const r = (hex >> 16) & 255;
+  const g = (hex >> 8) & 255;
+  const b = hex & 255;
+  const amt = percent / 100;
+
+  const newR = Math.min(255, Math.round(r + 255 * amt));
+  const newG = Math.min(255, Math.round(g + 255 * amt));
+  const newB = Math.min(255, Math.round(b + 255 * amt));
+
+  return `rgb(${newR}, ${newG}, ${newB})`;
 }
